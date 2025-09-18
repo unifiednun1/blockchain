0df65e85f4f41555328227454384883f8c8c5409
bitnunblockchain-ux <info@unifiednun.com>
2025-09-18 21:48:53 +0200

    feat: enable global mining by adding useActionMining to _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,8 @@
+
 import '../styles/global.css';
+import useActionMining from '../hooks/useActionMining';
 
 export default function App({ Component, pageProps }) {
+  useActionMining();
   return <Component {...pageProps} />;
 }
